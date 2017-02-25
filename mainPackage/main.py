42d11bc93ff17202f9ffd7a4ed1793608b62fbc4
Daniel McMahon <daniel40392@gmail.com>
2017-02-25 13:00:55 +0000

    TurnOn Functionality Added... still testing.

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -5,18 +5,19 @@ Created on Feb 24, 2017
 '''
 # a = first set of co-ords
 # b = second set of co-ords
-def turnOn(a,b,c,d):
+def turnOn(a,b,c,d,LEDgrid):
     '''Function intended to handle turnOn feature'''
     output = []
-    #for elem in range (a,b+1):
-        #if element is in range, make it True i.e. Turn on light
-        #ignroing if light is already on
-        #elem == True;
-        #output += elem
-    return output;
+    #x refers to column
+    for x in range(0,3):
+        #y refers to row
+        for y in range (1,5):
+            LEDgrid[x][y] = True
+        
+    return LEDgrid;
 
 
-def switch(a,b):
+def switch(a,b,c,d,LEDgrid):
     '''Function intended to switch on or off lights'''
     output = []
     for elem in range (a,b+1):
@@ -30,81 +31,92 @@ def switch(a,b):
     return output
 
 
-def turnOff(a,b):
+def turnOff(a,b,c,d,LEDgrid):
     '''Function intended to handle turnOff feature'''
     output = []
-    for elem in range (a,b+1):
+    #for elem in range (a,b+1):
         #if element is in range, make it False i.e. Turn off light
         #ignoring if light is already off
-        elem = False;
-        output += elem
+        #elem = False;
+        #output += elem
     return output
 
 
-def fileRead(file):
-    '''Function intended to read key data from file'''
-    '''initial testing without command line input'''
+def fileReadL(file):
     with open(file) as file:
         x = file.readline()
         L = int(x)
-        #return L
+        L = 10
+        return L
+    
+def fileRead(file,L):
+    '''Function intended to read key data from file'''
+    '''initial testing without command line input'''
+    with open(file) as file:
         for line in file:
-            
             if line.startswith("turn on"):
-                print("Turn on detected")
-                print(line.split(" ")[2])
-                print(line.split(" ")[4])
-                print()
-                #a = 
-                #c =
-                #c =
-                #d =
-                #turnOn(a,b,c,d)
+                ac = line.split(" ")[2]
+                bd = line.split(" ")[4]
+                
+                #a = int(ac.split(",")[0])
+                #c = int(ac.split(",")[1])
+                
+                #b = int(bd.split(",")[0])
+                #d = int(bd.split(",")[1])
+                
+                a,b,c,d = 5,6,7,8
+                
+                turnOn(a,b,c,d,L)
                 
                 
             elif line.startswith("switch"):
-                print("Switch detected")
-                print(line.split(" ")[1])
-                print(line.split(" ")[3])
-                print()
-                #a = (w,x); #where w and x are the first two int values on the line
-                #b = (y,z); #where y and z are the second two in values on the line
-                #switch(a,b)
+                ac = line.split(" ")[1]
+                bd = line.split(" ")[3]
+            
+                a = int(ac.split(",")[0])
+                c = int(ac.split(",")[1])
+                
+                b = int(bd.split(",")[0])
+                d = int(bd.split(",")[1])
+                #switch(a,b,c,d)
                 
                 
             elif line.startswith("turn off"):
-                print("Turn off detected")
-                print(line.split(" ")[2])
-                print(line.split(" ")[4])
-                print()
-                #a = (w,x); #where w and x are the first two int values on the line
-                #b = (y,z); #where y and z are the second two in values on the line
-                #turnOff(a,b)
                 
-    
-        
-        
-        #for line in file:
-        #   print(line)
+                ac = line.split(" ")[2]
+                bd = line.split(" ")[4]
+                
+                a = int(ac.split(",")[0])
+                c = int(ac.split(",")[1])
+                
+                b = int(bd.split(",")[0])
+                d = int(bd.split(",")[1])
+                #turnOff(a,b,c,d)
 
 
 def plotMap(L):
     '''function to plot the map for testing purposes'''
-    line = [False] * (L-1)
-    output = []
+    ## encountered functional error by using variable...
+    ## line = [False] * (L-1)
+    plottedMap = []
     for i in range(0,L-1):
-        output.append(line)
-    
-    for elem in output:
-        print(elem)
-     
-    return output
+        plottedMap.append([False] * (L-1))
+        
+    return plottedMap
     
 def main():
     '''main function'''
-    q = fileRead("input_assign3.txt")
-
+    L = fileReadL("input_assign3.txt")
+    LEDgrid = plotMap(L)
     
+    for item in LEDgrid:
+        print( item )
+    print()
+    
+    fileRead("input_assign3.txt",LEDgrid)
+    
+    for item in LEDgrid:
+        print(item)
     
 if __name__ == '__main__':
     main()
