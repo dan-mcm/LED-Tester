677a69eff43e0ab338d8ef9b6c75954fed93ef5b
Daniel McMahon <daniel40392@gmail.com>
2017-02-25 16:45:39 +0000

    Minor syntax change

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 '''
 Created on Feb 24, 2017
 
-@author: danie
+@author: Daniel McMahon
 '''
+from setuptools import setup
+
+setup(name="ledtester",
+      version="0.1",
+      description="LED Testing for Assignment 3 in COMP30670 2017",
+      url="",
+      author="Daniel McMahon",
+      author_email="daniel.mcmahon2@ucdconnect.ie",
+      license="GPL3",
+      packages=['src'],
+      entry_points={
+          'console_scripts':['led_tester=src.main:main']
+          },
+      install_requires=[
+          ],
+      )
