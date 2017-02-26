637183072f8e8521589c242ae8a97b8f5a144dfb
Daniel McMahon <daniel40392@gmail.com>
2017-02-26 13:30:59 +0000

    Turned out I had ab,cd the wrong way in the applyChanges function... D'oh! Still not 100% accurate but much closer now.

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ Created on Feb 24, 2017
 '''
 
 import argparse
+import urllib.request
+from astropy._erfa.core import ab
 
 def turnOn(a,b,c,d,LEDgrid):
     '''Function intended to handle turnOn feature. Turns lights on (or 'true') regardless of their current state'''
     for column in range(a,c):
         for row in range (b,d):
             LEDgrid[column][row] = True
-        
     return LEDgrid
 
 def switch(a,b,c,d,LEDgrid):
@@ -36,55 +37,59 @@ def turnOff(a,b,c,d,LEDgrid):
 
 def fileReadL(file):
     '''Function that reads the value of L from a text file - this will determine size of LEDgrid'''
-    with open(file) as file:
-        x = file.readline()
+    #this feels a little cheaty.... returns after first line is iterated in loop!
+    for line in file.split('\n'):
+        x = line
         L = int(x)
         return L
     
 def applyChanges(file,L):
     '''Function that parses input from file and carries out various functions according to strings starting values'''
-    
-    with open(file) as file:
-        for line in file:
-            if line.startswith("turn on"):
-                ac = line.split(" ")[2]
-                bd = line.split(" ")[4]
+
+    for line in file.split('\n'):
+        if line.startswith("turn on"):
+            ab = line.split(" ")[2]
+            cd = line.split(" ")[4]
                 
-                a = int(ac.split(",")[0])
-                c = int(ac.split(",")[1])
+            a = int(ab.split(",")[0])
+            b = int(ab.split(",")[1])
                 
-                b = int(bd.split(",")[0])
-                d = int(bd.split(",")[1])
+            c = int(cd.split(",")[0])
+            d = int(cd.split(",")[1])
+
+            x=turnOn(a,b,c,d,L)
+            L=x
                 
-                turnOn(a,b,c,d,L)
                 
-                
-            elif line.startswith("switch"):
-                ac = line.split(" ")[1]
-                bd = line.split(" ")[3]
+        elif line.startswith("switch"):
+            ab = line.split(" ")[1]
+            cd = line.split(" ")[3]
             
-                a = int(ac.split(",")[0])
-                c = int(ac.split(",")[1])
+            a = int(ab.split(",")[0])
+            b = int(ab.split(",")[1])
                 
-                b = int(bd.split(",")[0])
-                d = int(bd.split(",")[1])
+            c = int(cd.split(",")[0])
+            d = int(cd.split(",")[1])
                 
-                switch(a,b,c,d,L)
+            y=switch(a,b,c,d,L)
+            L=y
                 
+        elif line.startswith("turn off"):
                 
-            elif line.startswith("turn off"):
+            ab = line.split(" ")[2]
+            cd = line.split(" ")[4]
                 
-                ac = line.split(" ")[2]
-                bd = line.split(" ")[4]
+            a = int(ab.split(",")[0])
+            b = int(ab.split(",")[1])
                 
-                a = int(ac.split(",")[0])
-                c = int(ac.split(",")[1])
+            c = int(cd.split(",")[0])
+            d = int(cd.split(",")[1])
                 
-                b = int(bd.split(",")[0])
-                d = int(bd.split(",")[1])
-                
-                turnOff(a,b,c,d,L)
+            z=turnOff(a,b,c,d,L)
+            L=z
     
+    return L
+
 def plotMap(L):
     '''function to create the starting LEDgrid'''
     plottedMap = []
@@ -96,31 +101,35 @@ def plotMap(L):
 def main(): 
     '''main function - reads console input and passes it through various functions and outputs total lights left on'''
     
-    #parsing the user file...
+    #parsing the user input - expecting a link from the users input...
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', help='input help')
     args = parser.parse_args()
-    filename = args.input
+    link = args.input #replace link with filename to read standard files
+    
+    #reading the link
+    req=urllib.request.urlopen(link)
+    buffer=req.read().decode('utf-8')
     
     #setting value L from file
-    L = fileReadL(filename)
+    L = fileReadL(buffer)#replace buffer with filename to read standard files
     
     #setting up grid based on L value
     LEDgrid = plotMap(L)
     
     #applying changes with turnOn,Switch,turnOff functions called in applyChanges function
-    applyChanges(filename,LEDgrid)
-    
+    changes=applyChanges(buffer,LEDgrid)#replace buffer with filename to read standard files
+    LEDgrid=changes
     #note: functions called within applyChanges return the updated LEDgrid
     #the below prints out total number of lights on by iterating through values of a list inside a list
     counter = 0
+    
     for line in LEDgrid:
         for value in line:
             if value == True:
                 counter += 1
     
-    print("Total number of lights on =", counter)
-    
+    print("Total number of lights on:", counter)
     
 if __name__ == '__main__':
     main()
